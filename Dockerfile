b344ae314bec313a9353068926bf6b2f7ba119db
Kerry Wilson <kerry@allthingswilson.com>
2018-03-19 17:25:10 -0500

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM nginx
+
+RUN echo "hello from ecs" > /usr/share/nginx/html/index.html
---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -xe
+
+export AWS_DEFAULT_REGION=us-east-1
+
+TEMPLATE_PKG=pipeline-package.yaml
+
+ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+
+# clean old
+if [ -f ${TEMPLATE_PKG} ]; then
+	rm ${TEMPLATE_PKG}
+fi
+
+# create new
+aws cloudformation package \
+	--template-file pipeline.yaml \
+	--s3-bucket kwilson.reluslabs.com \
+	--output-template-file ${TEMPLATE_PKG}
+
+# validate the template
+aws cloudformation validate-template --template-body file://${TEMPLATE_PKG}
+
+# deploy it
+aws cloudformation deploy \
+	--template-file ${TEMPLATE_PKG} \
+	--stack-name codepipeline-docker-ecs \
+	--capabilities CAPABILITY_NAMED_IAM \
+	--parameter-overrides AccountId=${ACCOUNT_ID} ApplicationName=ecs-docker Region=us-east-1
